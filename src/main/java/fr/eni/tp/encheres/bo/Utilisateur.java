611cd96069e18a4466344ee5793472de46c17e47
CamilleQuesnel <vanillagrance@hotmail.fr>
2025-04-28 11:58:10 +0200

    Second commit ajout de la BO

---
+++ src/main/java/fr/eni/tp/encheres/bo/Utilisateur.java
@@ -0,0 +1,16 @@
+package fr.eni.tp.encheres.bo;
+
+public class Utilisateur {
+    private long noUtilisateur;
+    private String pseudo;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private int codePostal;
+    private String ville;
+    private String motDePasse;
+    private int credit;
+    private boolean administrateur;
+}
