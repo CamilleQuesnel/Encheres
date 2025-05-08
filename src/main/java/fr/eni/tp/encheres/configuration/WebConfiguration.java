85aca2107c790f245a3b240efe93d75d78def65e
Sebastien Fischer <113098123+SebastienFischer29@users.noreply.github.com>
2025-05-08 09:25:41 +0200

    SEB: rectification liens images + affichage heure

--- src/main/java/fr/eni/tp/encheres/configuration/WebConfiguration.java
+++ src/main/java/fr/eni/tp/encheres/configuration/WebConfiguration.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration  implements WebMvcConfigurer {
 
-    //@Value("${file.upload-dir}")
-    //private String uploadDir;
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,7 +16,7 @@ public class WebConfiguration  implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**")
 //                    .addResourceLocations("file:///static/images/")
 //                    .addResourceLocations("file:///public/images/")
-                    .addResourceLocations("file:///E:/Encheres/")
+                    .addResourceLocations("file:///" + uploadDir)
                     .addResourceLocations("classpath:/static/images/") // Garder l'accès aux ressources statiques
                     .resourceChain(true);
 
